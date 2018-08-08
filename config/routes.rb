df484f69ccdeae106796d44dfa6a5f7cb932016c
Silent Owl <nikolas.net.mail@gmail.com>
2018-08-08 15:35:44 +0300

    Add registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => {:registrations => "registrations"}
   root to: 'pages#index'
-  
   devise_scope :user do
   	get 'login', to: 'devise/sessions#new'
   end
+
 end
+
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
