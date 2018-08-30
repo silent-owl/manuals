c36ad5cf2020e9525ac0da79638bafafaf66fa40
Silent Owl <nikolas.net.mail@gmail.com>
2018-08-30 16:42:43 +0300

    Fixed devise registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
+  devise_for :users, :controllers => {registrations: 'registrations', :omniauth_callbacks => "users/omniauth_callbacks"}
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     root to: 'pages#index'
     devise_scope :user do
