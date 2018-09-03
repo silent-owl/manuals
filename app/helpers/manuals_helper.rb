ad311f1166701ad86a0936640067c560e8adfda8
Silent Owl <nikolas.net.mail@gmail.com>
2018-09-03 12:40:20 +0300

    Added brand icon. Deletedcomment in manuals_helper

--- app/helpers/manuals_helper.rb
+++ app/helpers/manuals_helper.rb
@@ -1,15 +1,4 @@
 module ManualsHelper
-  # def manual_nav_panel_path
-  #     if user_signed_in?
-  #       if ((current_user.role == "admin") || current_user.id == @manual.user.id)
-  #       'manuals/show/nav_manual_panel/owner_admin_panel'
-  #       else
-  #         'manuals/show/nav_manual_panel/owner_admin_panel'
-  #       end   
-  #     else
-  #       'manuals/show/nav_manual_panel/owner_admin_panel' 
-  #     end
-  # end 
 
   def has_content_path
       if (@manual.steps.count >= 1)
